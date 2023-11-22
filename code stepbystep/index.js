3a74391806174c696e3339bfde92eeda1f7a8e86
parakhakshay <parakhakshay9@gmail.com>
2023-11-22 22:28:18 +0530

    Node
    Practice and Different IDEAS

---
+++ code stepbystep/index.js
@@ -0,0 +1,24 @@
+
+
+console.log("I love NodeJS");
+// Can be Asked in the interview.. whether browser's console.log and nodeJS console.log is same or different!!Answer is below:
+// note console.log in VScode/CMD or nodeJS is different from browser because
+// nodeJS runtime variable which means console.log is also separated from the Java to create NodeJS own module
+// Written in NodeJs Documentation or checkout the image named nodeJSConsloe.png
+
+
+
+// when ever statement is not returning anything then it will print the o/p with saying undefined:
+// console.log("U love NodeJS")
+// o/p:
+// U love NodeJS
+// undefined
+
+// ******don't consider this undefined as an error, because it's not the error u can assume this as a notification********
+
+
+let v=20;
+let b="20";
+
+console.log(a=b+v);
+console.log(typeof(a));
---
+++ noteAPP.js
@@ -0,0 +1,7 @@
+const command=process.argv[2];
+
+if (command==='add'){
+    console.log("Adding a note!!");
+}else if(command==='remove'){
+    console.log("Removing a note!");
+}
