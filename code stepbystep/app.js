53b187458071b86f59cfe7e8ed2e54a2d3affe5c
parakhakshay <parakhakshay9@gmail.com>
2023-12-08 22:30:29 +0530

    nodeJS codeStepbyStep

---
+++ code stepbystep/app.js
@@ -0,0 +1,17 @@
+
+// exporting elements form this file to another file
+
+module.exports={
+
+    x:800,
+    y:500,
+
+    // creating a function inside the object
+    z:function(){
+        return 10;
+    }
+
+
+}
+
+
--- code stepbystep/index.js
+++ code stepbystep/index.js
@@ -22,3 +22,56 @@ let b="20";
 
 console.log(a=b+v);
 console.log(typeof(a));
+
+
+
+
+var x=20; 
+
+if (x===20){
+    console.log("matched");
+}
+//  difference between === and == when we use === then it will check type and value both before executing the program further.
+// but in the case of the == it only check value not the type of the value
+
+
+
+let result="";
+
+for (let index = 0; index < 10; index++) {
+    result=result+index+",";
+    
+    
+    
+}
+console.log(result);
+console.log(result.length);
+var result2= result.slice(0,19)
+
+console.log(result2);
+
+
+let arrays=[0,1,2,3,45,6,78,];
+
+console.log(arrays);
+console.log(arrays[5]);
+
+
+
+// import from another file
+
+const app=require('./app')
+console.log(app.x);
+console.log(app.y);
+console.log(app.z());
+console.log(app);
+
+
+// interview question what is filter and why we use filter
+// filter is used to extract the specific desired value from the array
+
+result_filtered_arrray=arrays.filter((element)=>{
+    return element>=3 ;
+});
+
+console.log(result_filtered_arrray);
